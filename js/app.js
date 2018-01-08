059e4373bbfbebae340949ab0b5d412e3e0a3f54
Betto <bettosgames@gmail.com>
2018-01-07 21:19:42 -0400

    añadido de documentos

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function evento_onload(){
+	alert("Bienvenido a nuestro formulario de registro de usuario.");
+}
+function evento_onclick(){
+	alert("Los datos fueron enviados exitosamente");
+}
+function evento_onfocus(elemento){
+	elemento.style.background="#CCCCCC"
+}
+function evento_oncopy(){
+	alert("El texto ha sido copiado");
+}
