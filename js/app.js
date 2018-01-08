340056f7b6304ecb3ded4ace9d7863a874ce67f2
Betto <bettosgames@gmail.com>
2018-01-08 02:22:30 -0400

    tres botones
    fallas en el cuarto botón

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var listaEstudiantes=[
 {"codigo":"A003", "nombre":"José Pérez", "nota":96},
 {"codigo":"A004", "nombre":"Jorge de la Rocha", "nota":48},
 {"codigo":"A005", "nombre":"Susana Mendieta", "nota":84},
-{"codigo":"A006", "nombre":"Natalia Ortega", "nota":23},
+{"codigo":"A006", "nombre":"Natalia Ortega", "nota":43},
 {"codigo":"A007", "nombre":"Juana Ortiz", "nota":100},
 {"codigo":"A008", "nombre":"Carla Sánchez", "nota":98},
 {"codigo":"A009", "nombre":"Horacio Portugal", "nota":58},
@@ -42,17 +42,38 @@ function mostrarPromedio() {
     Promediar(listaEstudiantes);
 };
 
-//Función para mostrar la nota mpas alta
-function Promediar(json) {
-	var sumarNotas=0;
+//Función para mostrar la nota más alta
+function NotaMasAlta(json) {
+	var mayor=-1;
 	var i;
   for (i=0; i<json.length; i++) {
-    sumarNotas+=json[i].nota;
-  }
-  document.getElementById("promedio").innerHTML="El promedio del curso es: "+sumarNotas/json.length;
+		if (json[i].nota>mayor){
+			mayor=json[i].nota;
+			mejorEst=json[i].nombre;
+		};
+	}
+  document.getElementById("mejorNota").innerHTML="La mejor nota es de:  "+mejorEst;
 };
 
 //Función para mostrar la nota más alta
-function mostrarPromedio() {
-    Promediar(listaEstudiantes);
+function mostrarMejorNota() {
+    NotaMasAlta(listaEstudiantes);
+};
+
+//Función para mostrar la nota más baja
+function NotaMasBaja(json) {
+	var menor=101;
+	var i;
+  for (i=0; i<json.length; i++) {
+		if (json[i].nota<menor){
+			menor=json[i].nota;
+			//peorEst=json[i].nombre;
+		};
+	}
+  document.getElementById("peorNota").innerHTML="La peor nota es de:  "+menor;
+};
+
+//Función para mostrar la nota más baja
+function mostrarPeorNota() {
+    NotaMasBaja(listaEstudiantes);
 };
