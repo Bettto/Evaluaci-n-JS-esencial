492cdb427c69ab8a7c67d1b6af496aba8fcf3040
Betto <bettosgames@gmail.com>
2018-01-08 01:47:46 -0400

    segundo botón concluido

--- js/app.js
+++ js/app.js
@@ -1,84 +1,58 @@
 //Array de estudiantes
-var listaEstudiantes='{"estudiantes":['+
-'{"codigo":"A001", "nombre":"Luis Chavarria", "nota":78},'+
-'{"codigo":"A002", "nombre":"Elizabeth Ordoñez", "nota":87},'+
-'{"codigo":"A003", "nombre":"José Pérez", "nota":96},'+
-'{"codigo":"A004", "nombre":"Jorge de la Rocha", "nota":48},'+
-'{"codigo":"A005", "nombre":"Susana Mendieta", "nota":84},'+
-'{"codigo":"A006", "nombre":"Natalia Ortega", "nota":23},'+
-'{"codigo":"A007", "nombre":"Juana Ortiz", "nota":100},'+
-'{"codigo":"A008", "nombre":"Carla Sánchez", "nota":98},'+
-'{"codigo":"A009", "nombre":"Horacio Portugal", "nota":58},'+
-'{"codigo":"A010", "nombre":"Alicia Bustamante", "nota":95}'+
-']}';
-
-//Convertir el Array a JSON
-var JSONestudiantes=JSON.parse(listaEstudiantes);
+var listaEstudiantes=[
+{"codigo":"A001", "nombre":"Luis Chavarria", "nota":78},
+{"codigo":"A002", "nombre":"Elizabeth Ordoñez", "nota":87},
+{"codigo":"A003", "nombre":"José Pérez", "nota":96},
+{"codigo":"A004", "nombre":"Jorge de la Rocha", "nota":48},
+{"codigo":"A005", "nombre":"Susana Mendieta", "nota":84},
+{"codigo":"A006", "nombre":"Natalia Ortega", "nota":23},
+{"codigo":"A007", "nombre":"Juana Ortiz", "nota":100},
+{"codigo":"A008", "nombre":"Carla Sánchez", "nota":98},
+{"codigo":"A009", "nombre":"Horacio Portugal", "nota":58},
+{"codigo":"A010", "nombre":"Alicia Bustamante", "nota":95}
+];
 
 //Crear función para leer el JSON
 function leerJSON(json){
 	var salida="---Planilla de Estudiantes---<br>";
 	var i;
-	for(i=0;i<json.estudiantes.length;i++){
-		salida+="Código: "+json.estudiantes[i].codigo+" - "+"Nombre: "+json.estudiantes[i].nombre+" - "+"Calificación: "+json.estudiantes[i].nota+"<br>";
+	for(i=0; i<json.length; i++){
+		salida+="Código: "+json[i].codigo+" - "+"Nombre: "+json[i].nombre+" - "+"Calificación: "+json[i].nota+"<br>";
 	}
 	document.getElementById("planilla").innerHTML=salida;
 };
 
 //Función para mostrar la planilla de estudiantes
 function mostrarPlanilla() {
-    leerJSON(JSONestudiantes);
+    leerJSON(listaEstudiantes);
 };
 
-
-/*
-//Función para calcular el promedio
-function average(array) {
-  function plus(a, b) { return a + b; }
-  //console.log(array.reduce(plus)) = 1295
-  return array.reduce(plus) / array.length;
-};
-console.log(average(JSONestudiantes.map(json.estudiantes.nota)));
-
-/*
-var Avgmarks = 0;
-
-for (var i=0; i < json.estudiantes.length; i++) {
-        Avgmarks += JSONestudiantes[i][1];
-        var avg = (Avgmarks/JSONestudiantes.length);
-}
-document.getElementById("promedio").innerHTML=("Average grade: " + (Avgmarks)/JSONestudiantes.length);
-
-
-/*
-function average(array) {
-  function plus(a, b) { return a + b; }
-  //console.log(array.reduce(plus)) = 1295
-  return array.reduce(plus) / array.length;
-}
-function age(p){return p.nota};
-console.log(average(JSONestudiantes.map(age)));
-/*
-function calcularPromedio(json){
-	function sumarNotas(a, b) {
-		return a+b;
-	}
-	return json(sumarNotas)/array.length;
-};
-console.log(calcularPromedio(JSONestudiantes.map(json.estudiantes.nota)));
-	var salida="El promedio del curso es: "
+//Función para calcular el Promedio
+function Promediar(json) {
 	var sumarNotas=0;
-	for (i=0;i<json.length;i++){
-		sumarNotas+=json[i].nota;
-		return sumarNotas/json.length;
-
-	}
-
-
-
-//Función para mostrar el promedio
-function mostrarPromedio() {
-    document.getElementById("promedio").innerHTML=calcularPromedio(JSONestudiantes);
+	var i;
+  for (i=0; i<json.length; i++) {
+    sumarNotas+=json[i].nota;
+  }
+  document.getElementById("promedio").innerHTML="El promedio del curso es: "+sumarNotas/json.length;
 };
-*/
-console.table (JSONestudiantes);
+
+//Función para mostrar la planilla de estudiantes
+function mostrarPromedio() {
+    Promediar(listaEstudiantes);
+};
+
+//Función para mostrar la nota mpas alta
+function Promediar(json) {
+	var sumarNotas=0;
+	var i;
+  for (i=0; i<json.length; i++) {
+    sumarNotas+=json[i].nota;
+  }
+  document.getElementById("promedio").innerHTML="El promedio del curso es: "+sumarNotas/json.length;
+};
+
+//Función para mostrar la nota más alta
+function mostrarPromedio() {
+    Promediar(listaEstudiantes);
+};
