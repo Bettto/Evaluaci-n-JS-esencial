d550646d7c829b77f2f869655fe9c3e3c1842da3
Betto <bettosgames@gmail.com>
2018-01-08 00:12:56 -0400

    Revert "primer botón exitoso"
    This reverts commit f28a89e83ba9253e0af02f6cad58c8996f8b0e45.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
 <<<<<<< HEAD
 //Array de estudiantes
-var listaEstudiantes=[
-{"codigo":"A001", "nombre":"Luis Chavarria", "nota":78},
-{"codigo":"A002", "nombre":"Elizabeth Ordoñez", "nota":87},
-{"codigo":"A003", "nombre":"José Pérez", "nota":96},
-{"codigo":"A004", "nombre":"Jorge de la Rocha", "nota":48},
-{"codigo":"A005", "nombre":"Susana Mendieta", "nota":84},
-{"codigo":"A006", "nombre":"Natalia Ortega", "nota":23},
-{"codigo":"A007", "nombre":"Juana Ortiz", "nota":100},
-{"codigo":"A008", "nombre":"Carla Sánchez", "nota":98},
-{"codigo":"A009", "nombre":"Horacio Portugal", "nota":58},
-{"codigo":"A010", "nombre":"Alicia Bustamante", "nota":95}
-];
+var listaEstudiantes='{"estudiantes":['+
+'{"codigo":"A001", "nombre":"Luis Chavarria", "nota":78},'+
+'{"codigo":"A002", "nombre":"Elizabeth Ordoñez", "nota":87},'+
+'{"codigo":"A003", "nombre":"José Pérez", "nota":96},'+
+'{"codigo":"A004", "nombre":"Jorge de la Rocha", "nota":48},'+
+'{"codigo":"A005", "nombre":"Susana Mendieta", "nota":84},'+
+'{"codigo":"A006", "nombre":"Natalia Ortega", "nota":23},'+
+'{"codigo":"A007", "nombre":"Juana Ortiz", "nota":100},'+
+'{"codigo":"A008", "nombre":"Carla Sánchez", "nota":98},'+
+'{"codigo":"A009", "nombre":"Horacio Portugal", "nota":58},'+
+'{"codigo":"A010", "nombre":"Alicia Bustamante", "nota":95}'+
+']}';
 
+//Convertir el Array a JSON
+var JSONestudiantes=JSON.parse(listaEstudiantes);
+
+//Crear función para leer el JSON
 function leerJSON(json){
 	var salida="---Planilla de Estudiantes---<br>";
 	var i;
-	for(i=0;i<json.length;i++){
-		salida+="Código: "+json[i].codigo+" - "+"Nombre: "+json[i].nombre+" - "+"Calificación: "+json[i].nota+"<br>";
+	for(i=0;i<json.estudiantes.length;i++){
+		salida+="Código: "+json.estudiantes[i].codigo+" - "+"Nombre: "+json.estudiantes[i].nombre+" - "+"Calificación: "+json.estudiantes[i].nota+"<br>";
 	}
 	document.getElementById("planilla").innerHTML=salida;
 }
 
 //Función para mostrar la planilla de estudiantes
 function mostrarPlanilla() {
-    leerJSON(listaEstudiantes);
+    leerJSON(JSONestudiantes);
 }
 
-/*//Función para calcular el promedio
+//Función para calcular el promedio
 function calcularPromedio(json){
 	var salida="El promedio del curso es: "
 	var sumarNotas=0;
@@ -38,12 +42,13 @@ function calcularPromedio(json){
 	}
 
 }
-*/
+
 //Función para mostrar el promedio
 function mostrarPromedio() {
     document.getElementById("promedio").innerHTML=calcularPromedio(JSONestudiantes);
 }
 
+<<<<<<< HEAD
 console.table (listaEstudiantes);
 =======
 function evento_onload(){
@@ -59,3 +64,6 @@ function evento_oncopy(){
 	alert("El texto ha sido copiado");
 }
 >>>>>>> parent of 95d0c86... primer botón
+=======
+console.table (JSONestudiantes);
+>>>>>>> parent of f28a89e... primer botón exitoso
